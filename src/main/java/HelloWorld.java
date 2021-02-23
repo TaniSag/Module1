acab0d6d900a11fd2d86a46c65ec6e30f3ebaa3c
TaniSag <t6346616@gmail.com>
2021-02-23 10:45:24 +0200

    add name

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello new world");
-        System.out.println("Second print");
+        System.out.println("Hello new world form me");
     }
 }
